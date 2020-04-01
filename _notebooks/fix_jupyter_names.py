e6b9a824bf376af473c3982d2b8b54bf83f91f06
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-04-01 18:48:14 -0300

    Add projections

--- _notebooks/fix_jupyter_names.py
+++ _notebooks/fix_jupyter_names.py
@@ -5,7 +5,7 @@ jupyter_fnames = glob.glob('*.ipynb')
 today = datetime.datetime.today().strftime("%Y-%m-%d")
 
 jupyter_new_fnames = [fname.replace('_','-') for fname in jupyter_fnames]
-jupyter_new_fnames = [fname.replace("2020-MES-DIA",today) for fname in jupyter_new_fnames]
+jupyter_new_fnames = [fname.replace("ANO-MES-DIA",today) for fname in jupyter_new_fnames]
 
 for old_fname, new_fname in zip(jupyter_fnames, jupyter_new_fnames):
     os.system(f'mv {old_fname} {new_fname}')
