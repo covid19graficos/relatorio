7f6c664f60d711a0df671a4772c7f22b9aa7d0a8
Fernando Wittmann <fernando.wittmann@gmail.com>
2020-04-02 16:04:47 -0300

    update charts and name fixer

--- _notebooks/fix_jupyter_names.py
+++ _notebooks/fix_jupyter_names.py
@@ -2,10 +2,12 @@ import os, glob
 import datetime
 
 jupyter_fnames = glob.glob('*.ipynb')
-today = datetime.datetime.today().strftime("%Y-%m-%d")
+today = datetime.datetime.today()
+yesterday = today - datetime.timedelta(days=1)
+yesterday = yesterday.strftime("%Y-%m-%d")
 
 jupyter_new_fnames = [fname.replace('_','-') for fname in jupyter_fnames]
-jupyter_new_fnames = [fname.replace("ANO-MES-DIA",today) for fname in jupyter_new_fnames]
+jupyter_new_fnames = [fname.replace("ANO-MES-DIA", yesterday) for fname in jupyter_new_fnames]
 
 for old_fname, new_fname in zip(jupyter_fnames, jupyter_new_fnames):
     os.system(f'mv {old_fname} {new_fname}')
